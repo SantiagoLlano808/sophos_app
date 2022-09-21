184ef9a2d23e8a281af7fe0d74bfffef2e93f672
Karen Maldonado <Desarrollo ITCC@DESKTOP-KADK26L>
2022-09-21 18:17:07 -0500

    Se agregan modulos de ping_pong

---
+++ lib/sophos_app/ping_pongDistr.ex
@@ -0,0 +1,15 @@
+def mailbox() do
+    IO.inspect self()
+    receive do
+      {:ping, process1, process2} ->
+        :timer.sleep(500)
+        IO.puts " Ping !!"
+        send(process2, {:pong, process1, process2})
+        mailbox()
+      {:pong, process1, process2} ->
+        :timer.sleep(500)
+        IO.puts " Pong !!"
+        send(process1, {:ping, process1, process2})
+        mailbox()
+    end
+  end
